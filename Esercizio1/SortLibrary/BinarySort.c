86ea0fd1433f8897955e0dcf1ff33c1fffbaf95a
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2021-03-23 16:02:13 +0100

    h files

---
+++ Esercizio1/SortLibrary/BinarySort.c
@@ -0,0 +1,11 @@
+#include "BinarySort.h"
+
+void BinaryInsertionSort(int *Array , int ArrayLength){
+    int i;
+    int position;
+    int tmp;
+    for(i=1;i<ArrayLength, i++){
+        position = BinarySearchPosition(Array, Array[i], 0, i);
+        tmp = 
+    }
+}
---
+++ Esercizio1/SortLibrary/BinarySort.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_REQ
+    #define SEARCH_REQ
+    #include <stdio.h>
+    #include <stdlib.h>
+#endif
+
+void BinaryInsertionSort(int *Array, int ArrayLength);
+
+void BinarySearchPosition(int Array, int x, int i, int j); 
