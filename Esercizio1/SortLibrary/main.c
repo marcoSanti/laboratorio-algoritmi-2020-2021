95b4b280377c49d26f3d0b22f2d94f86056b8d36
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2021-03-25 11:13:52 +0100

    not working with 20 element array...

--- Esercizio1/SortLibrary/main.c
+++ Esercizio1/SortLibrary/main.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char* argv[]) {
     int i;
-    int arr1[] = {11,10,9,8,7,6,5,4,3,2,1};
+    int arr1[] = {11,10,9,8,7,6,5,4,3,2,1,20,19,18,17,16,15,14,13,12};
     
-    for(i=0; i<11; i++) {
+    for(i=0; i<20; i++) {
         printf("[%d]", arr1[i]);
     }
     printf("\n");
-    MergeSort(arr1, 0, 10);
-    for(i=0; i<11; i++) {
+    MergeSort(arr1, 0, 20);
+    for(i=0; i<20; i++) {
         printf("[%d]", arr1[i]);
     }
     printf("\n");
