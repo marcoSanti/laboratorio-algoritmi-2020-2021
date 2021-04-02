0f5af660c8d12ef5701367d87496d6fc5379743b
marco <marco.santimaria@live.it>
2021-04-02 10:14:46 +0200

    added few comments on the function description and made better code

--- Esercizio1/Headers/MergeBinaryInsertionSort.h
+++ Esercizio1/Headers/MergeBinaryInsertionSort.h
@@ -7,10 +7,15 @@
     #include <errno.h>
     #include <unistd.h>
     #include <time.h>
-    //Length of sub array at wich point merge sort is swapped with binary insertion sort
-    #define K 45
+    #define K 45 //Length of sub array at wich point merge sort is swapped with binary insertion sort
 #endif
 
+/*
+    This typedef is here to be able to pass the pointer to the function which will be doing the comparison.
+    This way, the algoritm will be able to work with whatever data structure you would think of as long as you define
+    a compare function for the data structure you have implemented.
+
+*/
 typedef int (*sortingCompareFunction)(void*, void*);
 
 /*
@@ -20,6 +25,7 @@ typedef int (*sortingCompareFunction)(void*, void*);
         - array: a pointer to an array of records to be sorted
         -l : the lower index bound of the element. When the function is called for the first time, it should be 0
         -r : the upper boundaries of the indexes of my array. When the function is called for the first time it should be the last valid index of the array (array.length -1)
+        -mySortingCompareFunction:  This is a pointer to the comparing function to be used into the method
 */
 void MergeBinaryInsertionSort(void** array, int l, int r, sortingCompareFunction mySortingCompareFunction);
 
@@ -31,6 +37,7 @@ void MergeBinaryInsertionSort(void** array, int l, int r, sortingCompareFunction
         -l: the begin index of the first sub array to be merged
         -m : the mid point wich both function as last index for the first sub array and the begon index for the second sub array
         -r : the upper bound of the second sub array to be sorted
+        -mySortingCompareFunction:  This is a pointer to the comparing function to be used into the method
 
     BE AWARE THAT THIS FUNCTION IS ONLY MEANT TO BE CALLED BY THE MergeBinaryInsertionSort METHOD
 */
@@ -42,6 +49,7 @@ void Merge(void** array, int l, int m, int r, sortingCompareFunction mySortingCo
         -array: a pointer to the array in wich the sub arrays should be taken
         -l : the beggining index of the array 
         -r : the end of the array to be sorted
+        -mySortingCompareFunction:  This is a pointer to the comparing function to be used into the method
     BE AWARE THAT THIS FUNCTION IS ONLY MEANT TO BE CALLED BY THE MergeBinaryInsertionSort METHOD
 */
 void BinaryInsertionSort(void** array , int l, int r, sortingCompareFunction mySortingCompareFunction);
@@ -53,6 +61,7 @@ void BinaryInsertionSort(void** array , int l, int r, sortingCompareFunction myS
         -x the element that needs to be inserte
         -i : the beggining index of the array 
         -j : the end of the array to be sorted
+        -mySortingCompareFunction:  This is a pointer to the comparing function to be used into the method
     BE AWARE THAT THIS FUNCTION IS ONLY MEANT TO BE CALLED BY THE MergeBinaryInsertionSort METHOD
 */
 int BinarySearchPosition(void** array, void* x, int i, int j, sortingCompareFunction mySortingCompareFunction); 
--- Esercizio1/SortLibrary/main.c
+++ Esercizio1/SortLibrary/main.c
@@ -4,12 +4,17 @@
 #define TIME_LIM 600
 
 /*
-    Struct with the pointer to the function that will be used inside the ordering functions
+    This struct keeps saved the preferences of the sorting algorithm. It saves the pointer to the function 
+    that will be used to sort the array.
+    It also save the order in which the sorting will occur (ex: non decreasing or non increasing)
 */
 typedef struct {
     sortingCompareFunction comparePreference;
 } sortingPreferences;
 
+/*
+  This struct will be used to store the data coming from the file wich will be required to be sorted
+*/
 typedef struct {
     int id;
     char* string;
@@ -17,22 +22,31 @@ typedef struct {
     float numberFloat;
 } record;
 
-void arrayIsSorted(void** array, int size, sortingCompareFunction mySortingCompareFunction) {
-  int i, intSortedElements=0;
 
+/*
+  This function is used to chech wheter an array is sorted by a certain parameter wich is decided by the
+  mySortingCompareFunction pointer to compare function. 
+*/
+void arrayIsSorted(void** array, int size, sortingCompareFunction mySortingCompareFunction) {
+  int i;
   for(i = 0; i < size-1; i++) {
         if(mySortingCompareFunction(array[i], array[i+1]) > 0) {
             fprintf(stderr, "\n================\nNOT SORTED\n================\n");
             exit(EXIT_FAILURE);
         }
-        intSortedElements++;
     }
 }
  
+/*
+  This method compares two strings and returns the distances between the two
+*/
 int compareTwoString(record* firstRecord, record* secondRecord){
   return strcmp(firstRecord -> string, secondRecord -> string); 
 }
 
+/*
+  this function compares two functions and tell whether the first number is smaller than the second
+*/
 int compareTwoFloats(record* firstRecord, record* secondRecord){
   if(firstRecord->numberFloat <= secondRecord->numberFloat){
     return 0;
@@ -41,10 +55,18 @@ int compareTwoFloats(record* firstRecord, record* secondRecord){
   }
 }
 
+
+/*
+  this function compares two integer and return the difference between the two numbers
+*/
 int compareTwoIntegers(record* firstRecord, record* secondRecord){  
   return firstRecord->numberInt - secondRecord->numberInt;
 }
 
+/*
+  This function handles the signal.
+  in particular, when 10 minutes have elapsed, it will kill the process
+*/
 void signalHandler(int signal) {
   switch(signal) {
     case SIGALRM:
@@ -55,21 +77,24 @@ void signalHandler(int signal) {
 }
 
 int main(int argc, char* argv[]) {
-    int insertionModeUser, sizeOfArray;
-    int line = 0, id, i;
-    double duration; 
-    clock_t start, stop;
-    char stringInFile[MAX_LEN];
-    int secondNumber;
+    int insertionModeUser, sizeOfArray,line = 0, id, i, secondNumber;
     float thirdNumber;
-    record** myRecord = (record**) malloc(sizeof(record*));
+    double duration; 
+    char stringInFile[MAX_LEN];
+    clock_t start, stop;
+    record** myRecord;
     sortingPreferences mySortingPreferences;
     record* singleElement; 
-    FILE* myFile = fopen("records.csv", "r");
+    FILE* myFile;
+
+    myRecord  = (record**) malloc(sizeof(record*));
+
+    myFile = fopen("records.csv", "r");
     if(myFile == NULL) {
         fprintf(stderr, "Error: %s", strerror(errno));
         return 0;
     }
+
     signal(SIGALRM, signalHandler);
 
     /*=====================================Handle user inputs=======================================*/
@@ -112,11 +137,15 @@ int main(int argc, char* argv[]) {
     start = clock();
     MergeBinaryInsertionSort((void** )myRecord, 0, sizeOfArray-1, mySortingPreferences.comparePreference);
     stop = clock();
+
     printf("Sorting complete...\nStarting Verification...\n");    
+
     arrayIsSorted((void**)myRecord, sizeOfArray, mySortingPreferences.comparePreference);
+
     /* The CLOCKS_PER_SEC MACRO is defined inside the time.h library */
     duration = ( double ) ( stop - start ) / CLOCKS_PER_SEC;
     fprintf(stdout, "Sorting completed in: %.2lf\n", duration);
+    
     free(myRecord);
     return 0;
 }
