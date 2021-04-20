89cfdb27c17953536b339f6e3215215a78785eb3
Nicolo Vanzo <nicolo.vanzo@edu.unito.it>
2021-04-20 12:01:42 +0200

    Added in main.c the fopen function

--- Esercizio2/Src/main.c
+++ Esercizio2/Src/main.c
@@ -1,5 +1,22 @@
 #include "editDistance.h"
 
 int main(int argc, char* argv[]) {
+    FILE *myFile;
+    char myStringInput[200];
+    const char mySeparationChar[4] = "\0";
+    char *myToken;
+
+    myFile = fopen("correctme.txt", "r");
+    if(myFile == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    while( fscanf(myFile, "%[^,]\n", myStringInput) != EOF) {
+        myToken = strtok(myStringInput, mySeparationChar);
+        
+            fprintf(stderr, " %s\n", myToken);
+
+            myToken = strtok(NULL, mySeparationChar);        
+    }
     return 0;
 }
