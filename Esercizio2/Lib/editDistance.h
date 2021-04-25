f7b632a31076fab2b3c0f8b0f6ed674bf069a61e
marco <marco.santimaria@live.it>
2021-04-25 16:16:36 +0200

    Exercise number two completed without unit test but otherwise working

--- Esercizio2/Lib/editDistance.h
+++ Esercizio2/Lib/editDistance.h
@@ -4,12 +4,18 @@
     #include <stdio.h>
     #include <stdlib.h>
     #include <limits.h>
+
     #define MIN(x, y) (((x) < (y)) ? (x) : (y))
     #define SEPARATOR " ,.:;-_\n"
+
+
+    int memoizationHelpMatrix[50][50]; //used for dynamic programming! 
+    
+
 #endif
 
 int edit_distance(char* s1, char* s2);
 
-int edit_distance_dyn(char* s1, char* s2);
-
 char** loadDictionary(char *fileName, int *dictionaryElements);
+
+int edit_distance_dyn(char* str1, char* str2, int strlen1, int strlen2);
--- Esercizio2/Src/main.c
+++ Esercizio2/Src/main.c
@@ -25,6 +25,11 @@ int main(int argc, char *argv[])
         {
             tokenizedInputFile = realloc(tokenizedInputFile, (i + 1) * sizeof(char *));
             tokenizedInputFile[i] = malloc(strlen(myToken)+1);
+
+             if(myToken[0]>=65 && myToken[0]<=90){ //converto prima lettera parola in minuscolo se maiuscola
+                 myToken[0]=myToken[0]+32;
+             }
+                
             strcpy(tokenizedInputFile[i], myToken);
             i++;
             myToken = strtok(NULL, SEPARATOR);
@@ -35,6 +40,7 @@ int main(int argc, char *argv[])
 
     dictionary = loadDictionary("dictionary.txt", &dictionaryElements);
 
+#ifdef PAINFUL
     for(i=0;i<tokenizedNumberLines;i++){ //scorro le parole dentro il file da correggere
         minEditDistance = INT_MAX;
         for(j=0;j<dictionaryElements;j++){
@@ -50,6 +56,33 @@ int main(int argc, char *argv[])
             printf("Word %s is correct.\n", tokenizedInputFile[i]);
         }
     }
+#else
+ 
+    for(i=0;i<tokenizedNumberLines;i++){
+        minEditDistance = INT_MAX;
+        for(j=0;j<dictionaryElements;j++){
+
+            memset(memoizationHelpMatrix, -1, sizeof memoizationHelpMatrix); //init matrice a -1
+            tmp = edit_distance_dyn(tokenizedInputFile[i], dictionary[j], strlen(tokenizedInputFile[i]), strlen(dictionary[j]));
+        
+          
+            if(tmp < minEditDistance){
+                minEditDistance = tmp;
+                positionOfMinEditDistance = j;
+            }
+
+            if(minEditDistance == 0) break;
+        }
+        if(minEditDistance != 0){
+            printf("Word %10s is probably wrong... Did you mean %s (editDistance=%d)?\n", tokenizedInputFile[i], dictionary[positionOfMinEditDistance], minEditDistance);
+        }else{
+            printf("Word %10s is correct.\n", tokenizedInputFile[i]);
+        }
+    }
+ 
+  
+
+#endif
 
     return 0;
 }
