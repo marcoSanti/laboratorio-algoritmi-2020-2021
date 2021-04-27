d29c3f3deb047a483701992cc8d8970ad17d0197
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2021-04-27 08:54:55 +0200

    added target to run code without memoization

--- Esercizio2/Lib/editDistance.c
+++ Esercizio2/Lib/editDistance.c
@@ -1,15 +1,19 @@
 #include "editDistance.h"
 
-int edit_distance_dyn(char* str1, char*str2){
+int edit_distance_dyn(char *str1, char *str2)
+{
   memset(memoizationHelpMatrix, -1, sizeof memoizationHelpMatrix);
   return _edit_distance_dyn(str1, str2, strlen(str1), strlen(str2));
 }
 
-int _edit_distance_dyn(char * str1, char * str2, int strlen1, int strlen2) {
+
+int _edit_distance_dyn(char *str1, char *str2, int strlen1, int strlen2)
+{
   int insert, canc, noOp;
 
   //control to not overflow the space of memoizationHelpMatrix
-  if (strlen1 > 50 || strlen2 > 50) {
+  if (strlen1 > 50 || strlen2 > 50)
+  {
     printf("Warning: not enough space to use memoization! Skipping word...\n");
     return INT_MAX;
   }
@@ -21,9 +25,12 @@ int _edit_distance_dyn(char * str1, char * str2, int strlen1, int strlen2) {
   if (memoizationHelpMatrix[strlen1 - 1][strlen2 - 1] != -1)
     return memoizationHelpMatrix[strlen1 - 1][strlen2 - 1];
 
-  if (str1[0] == str2[0]) {
+  if (str1[0] == str2[0])
+  {
     noOp = _edit_distance_dyn(str1 + 1, str2 + 1, strlen1 - 1, strlen2 - 1);
-  } else {
+  }
+  else
+  {
     noOp = INT_MAX;
   }
 
@@ -33,16 +40,21 @@ int _edit_distance_dyn(char * str1, char * str2, int strlen1, int strlen2) {
   return memoizationHelpMatrix[strlen1 - 1][strlen2 - 1] = MIN(insert, MIN(noOp, canc));
 }
 
-int edit_distance(char * s1, char * s2) {
+int edit_distance(char *s1, char *s2)
+{
   int noOp, canc, insert;
 
-  if (strlen(s1) == 0 || strlen(s2) == 0) {
+  if (strlen(s1) == 0 || strlen(s2) == 0)
+  {
     return strlen(s1) + strlen(s2);
   }
 
-  if (s1[0] == s2[0]) {
+  if (s1[0] == s2[0])
+  {
     noOp = edit_distance(s1 + 1, s2 + 1);
-  } else {
+  }
+  else
+  {
     noOp = INT_MAX;
   }
 
@@ -52,34 +64,38 @@ int edit_distance(char * s1, char * s2) {
   return MIN(noOp, MIN(canc, insert));
 }
 
-char ** loadDictionary(char * fileName, int * dictionaryElements) {
+char **loadDictionary(char *fileName, int *dictionaryElements)
+{
 
-  FILE * dictFP;
+  FILE *dictFP;
   char wordTmp[200];
-  char ** dictionary;
+  char **dictionary;
   int i = 0;
 
-  if (fileName == NULL) {
+  if (fileName == NULL)
+  {
     printf("No dictionary available... exiting...\n");
     exit(EXIT_FAILURE);
   }
 
   dictFP = fopen(fileName, "r");
 
-  if (dictFP == NULL) {
+  if (dictFP == NULL)
+  {
     printf("Unable to open file!\n");
     exit(EXIT_FAILURE);
   }
 
-  dictionary = malloc(i * sizeof(char * ));
-  while (fscanf(dictFP, "%s", wordTmp) != EOF) {
-    dictionary = realloc(dictionary, (i + 1) * sizeof(void * ));
+  dictionary = malloc(i * sizeof(char *));
+  while (fscanf(dictFP, "%s", wordTmp) != EOF)
+  {
+    dictionary = realloc(dictionary, (i + 1) * sizeof(void *));
     dictionary[i] = malloc(strlen(wordTmp) + 1);
     strcpy(dictionary[i], wordTmp);
     i++;
   }
 
   fclose(dictFP);
-  * dictionaryElements = i;
+  *dictionaryElements = i;
   return dictionary;
 }
--- Esercizio2/Src/main.c
+++ Esercizio2/Src/main.c
@@ -18,7 +18,8 @@ int main(int argc, char *argv[])
     positionOfMinEditDistance = 0;
 
     myFile = fopen("correctme.txt", "r");
-    if (myFile == NULL) {
+    if (myFile == NULL)
+    {
         printf("Unable to open source file to correct.\n");
         exit(EXIT_FAILURE);
     }
@@ -30,7 +31,7 @@ int main(int argc, char *argv[])
         tokenizedInputFile is a double pointer array. Every element of the array is a pointer to a string, 
         this sring is out token.
     */
-    tokenizedInputFile=malloc(i*sizeof(char *));
+    tokenizedInputFile = malloc(i * sizeof(char *));
     while (fscanf(myFile, "%s\n", myStringInput) != EOF)
     {
         myToken = strtok(myStringInput, SEPARATOR);
@@ -42,10 +43,11 @@ int main(int argc, char *argv[])
             /*
                 Get rid of the uppercased letters.
             */
-            if(myToken[0] >= 65 && myToken[0] <= 90){ 
+            if (myToken[0] >= 65 && myToken[0] <= 90)
+            {
                 myToken[0] = myToken[0] + 32;
             }
-                
+
             strcpy(tokenizedInputFile[i], myToken);
             i++;
             myToken = strtok(NULL, SEPARATOR);
@@ -56,23 +58,29 @@ int main(int argc, char *argv[])
 
     dictionary = loadDictionary("dictionary.txt", &dictionaryElements);
 
-#ifdef PAINFUL
+#ifdef noMemoization
     /*
         Solve the problem without memoisation. 
         In this case we don't use a matrix.
     */
-    for(i=0;i<tokenizedNumberLines;i++){ 
+    for (i = 0; i < tokenizedNumberLines; i++)
+    {
         minEditDistance = INT_MAX;
-        for(j=0;j<dictionaryElements;j++){
+        for (j = 0; j < dictionaryElements; j++)
+        {
             tmp = edit_distance(tokenizedInputFile[i], dictionary[j]);
-            if(tmp<minEditDistance){
+            if (tmp < minEditDistance)
+            {
                 minEditDistance = tmp;
                 positionOfMinEditDistance = j;
             }
         }
-        if(minEditDistance != 0){
+        if (minEditDistance != 0)
+        {
             printf("Word %s is probably wrong... Did you mean %s?\n", tokenizedInputFile[i], dictionary[positionOfMinEditDistance]);
-        }else{
+        }
+        else
+        {
             printf("Word %s is correct.\n", tokenizedInputFile[i]);
         }
     }
@@ -81,14 +89,16 @@ int main(int argc, char *argv[])
         In order to support the memoisation method to solve the problem we need a matrix that will be used
         to store the progressive results while comparing two strings.
     */
-    for(i=0;i<tokenizedNumberLines;i++){
+    for (i = 0; i < tokenizedNumberLines; i++)
+    {
         minEditDistance = INT_MAX;
-        for(j=0;j<dictionaryElements;j++){
+        for (j = 0; j < dictionaryElements; j++)
+        {
 
-             
             tmp = edit_distance_dyn(tokenizedInputFile[i], dictionary[j]);
-        
-            if(tmp < minEditDistance){
+
+            if (tmp < minEditDistance)
+            {
                 minEditDistance = tmp;
                 positionOfMinEditDistance = j;
             }
@@ -96,11 +106,15 @@ int main(int argc, char *argv[])
                 if minEditDistance is zero it means that my two words are equal, otherwise the loop
                 goes on and the algorithm keeps searching for the edit distance.
             */
-            if(minEditDistance == 0) break;
+            if (minEditDistance == 0)
+                break;
         }
-        if(minEditDistance != 0){
+        if (minEditDistance != 0)
+        {
             printf("Word %10s is probably wrong... Did you mean %s (editDistance=%d)?\n", tokenizedInputFile[i], dictionary[positionOfMinEditDistance], minEditDistance);
-        }else{
+        }
+        else
+        {
             printf("Word %10s is correct.\n", tokenizedInputFile[i]);
         }
     }
