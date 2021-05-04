982a193a6eb57b7a9e55989a597721f5dff2d0a7
Nicolo Vanzo <nicolo.vanzo@edu.unito.it>
2021-05-04 11:27:13 +0200

    Strange error in UnitTest

--- Esercizio3/Src/UnitTest.java
+++ Esercizio3/Src/UnitTest.java
@@ -1,10 +1,6 @@
 package src;
 import java.util.Comparator;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,15 +8,27 @@ import org.junit.Test;
 public class UnitTest {
     
     UnionFindSet<Integer> firstElem;
+    UnionFindSet<Integer> secondElem;
 
     @Before 
     public void createStruct() {
         firstElem = new UnionFindSet<>(5);
+        secondElem = new UnionFindSet<>(10);
     }
 
     @Test 
     public void testNewElement() {
         assertEquals(0, firstElem.GetRank());
         assertSame(firstElem, firstElem.GetParent());
+    }
+
+    @Test
+    public void testUnion() {
+        firstElem.Union(firstElem, secondElem);
+        assertSame(secondElem, firstElem.GetParent());
+        assertEquals(0, firstElem.GetRank());
+        assertEquals(1, secondElem.GetRank());
+        assertEquals(5, firstElem.GetValue());
+        assertEquals(10, firstElem.GetParent().GetValue());
     }
 }
