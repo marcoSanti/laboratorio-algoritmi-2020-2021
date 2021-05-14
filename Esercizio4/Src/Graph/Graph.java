5310245205c2628e874836a635e1935a76b0ddba
Nicolo Vanzo <nicolo.vanzo@edu.unito.it>
2021-05-14 17:00:37 +0200

    One operation is missing + Unit tests for new operations are missing + Adapt the program to work with isDirect = true to do

--- Esercizio4/Src/Graph/Graph.java
+++ Esercizio4/Src/Graph/Graph.java
@@ -1,24 +1,89 @@
 package Src.Graph;
 import java.util.HashMap;
 
+/* 
+ *  Our struct is made by two hashmaps:
+ *  ToDos: recupero degli archi dei grafi O(N);
+ *  Gestire l'isDirect
+ *  UnitTest
+ */
 public class Graph<T, G> {
-    private HashMap<T, Link<T, G>> myHashTable;
+    private HashMap<T, HashMap<T, G>> myHashTable;
+    private boolean isDirect = true;
+
+    public Graph(boolean isDirect) {
+        this.isDirect = isDirect;
+    }
 
     public Graph() {
-        myHashTable = new HashMap<T, Link<T, G>>();
+        myHashTable = new HashMap<T, HashMap<T,G>>();
     }
 
     public void AddNode(T key) {
-        myHashTable.put(key, new Link<T,G>(null, null, null));
+        myHashTable.put(key, new HashMap<T,G>());
     }
 
     public void AddLink(T from, T to, G weight) {
-        myHashTable.put(from, new Link<T, G>(weight, to, myHashTable.get(from)));
+        HashMap<T,G> tmpMap = myHashTable.get(from);
+        tmpMap.put(to, weight);
+        if(!isDirect) {
+            tmpMap = myHashTable.get(to);
+            tmpMap.put(from, weight);
+        }
     }
     /* 
      *  Returns true if node with the specified key exists, false otherwise 
      */
     public boolean HasNode(T key) {
         return myHashTable.containsKey(key);
+    }
+
+    public G GetWeight(T from, T to) {
+        HashMap<T,G> tmpMap = myHashTable.get(from);
+        return tmpMap.get(to);
+    }
+
+    public HashMap<T,G> GetAdiacentNodes(T key) {
+        return myHashTable.get(key);
+    }
+
+    /*
+     * Loop through the HashTable for the other nodes 
+     */
+    public void DeleteNode(T key) {
+        myHashTable.remove(key);
+    }
+
+    public void DeleteLink(T from, T to) {
+        myHashTable.get(from).remove(to);
+    }
+
+    public int GetNumberOfNode() {
+        return myHashTable.size();
+    }
+
+    /*
+     * Loop through the keys
+     */
+    public int GetNumberOfLinks() {
+        int sum = 0;
+        for(T i: myHashTable.keySet()) {
+            sum += myHashTable.get(i).size();
+        } 
+        return sum;
+    }
+
+    public T[] GetNodes() {
+        T nodeList[myHashTable.size()];
+        int k = 0;
+        for(T i: myHashTable.keySet()) {
+            nodeList[k] = i;
+            k++;
+        } 
+        return nodeList;
+    }
+
+    public boolean IsDirect() {
+        return this.isDirect;
     }
 }
--- Esercizio4/Src/UnitTest/UnitTest.java
+++ Esercizio4/Src/UnitTest/UnitTest.java
@@ -30,5 +30,15 @@ public class UnitTest {
         assertFalse(myHashmap.HasNode("Venezia"));
     }
 
-
+    @Test 
+    public void AddLinks() {
+        myHashmap.AddNode("Torino");
+        myHashmap.AddNode("Milano");
+        myHashmap.AddNode("Genova");
+        myHashmap.AddLink("Torino", "Milano", 150);
+        myHashmap.AddLink("Torino", "Genova", 100);
+        assertTrue(150 == myHashmap.GetWeight("Torino", "Milano"));
+        assertTrue(100 == myHashmap.GetWeight("Torino", "Genova"));
+        assertFalse(100000 == myHashmap.GetWeight("Torino", "Milano"));
+    }
 }
