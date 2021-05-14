aa3433a31707133e1834c9fcf63ea56e5c816a83
Nicolo Vanzo <nicolo.vanzo@edu.unito.it>
2021-05-14 16:06:22 +0200

    Getting an error in AddNode

--- Esercizio4/src/Graph/Graph.java
+++ Esercizio4/src/Graph/Graph.java
@@ -9,10 +9,16 @@ public class Graph<T, G> {
     }
 
     public void AddNode(T key) {
-        myHashTable.put(key, null);
+        myHashTable.put(key, new Link<T,G>(null, null, null));
     }
 
     public void AddLink(T from, T to, G weight) {
         myHashTable.put(from, new Link<T, G>(weight, to, myHashTable.get(from)));
+    }
+    /* 
+     *  Returns true if node with the specified key exists, false otherwise 
+     */
+    public boolean HasNode(T key) {
+        return myHashTable.containsKey(key);
     }
 }
--- Esercizio4/src/UnitTest/UnitTest.java
+++ Esercizio4/src/UnitTest/UnitTest.java
@@ -7,12 +7,24 @@ import org.junit.Test;
 
 
 public class UnitTest {
-    
-    public Graph<String,Integer> myHasmap; 
+    public Graph<String,Integer> myHashmap; 
 
     @Test
     public void CreateNullGraph() {
-        myHasmap = new Graph<String, Integer>();
-        assertTrue(myHasmap instanceof Graph<String,Integer>);
+        myHashmap = new Graph<String, Integer>();
+        assertTrue(myHashmap instanceof Graph<?,?>);
     }
+
+    @Test
+    public void AddNodes() {
+        myHashmap.AddNode("Torino");
+        myHashmap.AddNode("Milano");
+        myHashmap.AddNode("Genova");
+        assertTrue(myHashmap.HasNode("Torino"));
+        assertTrue(myHashmap.HasNode("Milano"));
+        assertTrue(myHashmap.HasNode("Genova"));
+        assertFalse(myHashmap.HasNode("Venezia"));
+    }
+
+
 }
