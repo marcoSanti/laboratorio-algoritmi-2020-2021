51e87551c8e7267568ef675b94dc1bc736f36581
NickVanzo <Nickvanzo437@gmail.com>
2021-05-19 16:58:27 +0200

    Added some unit tests

--- Esercizio4/Src/UnitTest/UnitTest.java
+++ Esercizio4/Src/UnitTest/UnitTest.java
@@ -5,6 +5,8 @@ import src.graph.*;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import src.kruskal.*;
+import src.unionfindset.*;
 
 
 /* 
@@ -12,61 +14,61 @@ import org.junit.Test;
  */
 
 public class UnitTest {
-    public Graph<String,Integer> myHashmap; 
-    public Graph<String,Integer> myHashmapDirect;
-    public Graph<String,Integer> myHashmapNotDirect;
+    public Graph<String,Integer> myGraph; 
+    public Graph<String,Integer> myGraphDirect;
+    public Graph<String,Integer> myGraphNotDirect;
 
     @Before
     public void CreateStruct() throws GraphExceptions {
-        myHashmap = new Graph<String, Integer>();
-        myHashmapDirect = new Graph<String, Integer>(true);
-        myHashmapNotDirect = new Graph<String, Integer>(false);
+        myGraph = new Graph<String, Integer>();
+        myGraphDirect = new Graph<String, Integer>(true);
+        myGraphNotDirect = new Graph<String, Integer>(false);
     }
 
     @Test
     public void CreateNullGraph() throws Exception{
-        assertTrue(myHashmap instanceof Graph<?,?>);
+        assertTrue(myGraph instanceof Graph<?,?>);
     }
 
     @Test
     public void AddNodes() throws Exception{
-        myHashmap.AddNode("Torino");
-        myHashmap.AddNode("Milano");
-        myHashmap.AddNode("Genova");
-        assertTrue(myHashmap.HasNode("Torino"));
-        assertTrue(myHashmap.HasNode("Milano"));
-        assertTrue(myHashmap.HasNode("Genova"));
-        assertFalse(myHashmap.HasNode("Venezia"));
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
+        assertTrue(myGraph.HasNode("Torino"));
+        assertTrue(myGraph.HasNode("Milano"));
+        assertTrue(myGraph.HasNode("Genova"));
+        assertFalse(myGraph.HasNode("Venezia"));
     }
 
     @Test 
     public void AddLinks() throws Exception {
-        myHashmap.AddNode("Torino");
-        myHashmap.AddNode("Milano");
-        myHashmap.AddNode("Genova");
-        myHashmap.AddLink("Torino", "Milano", 150);
-        myHashmap.AddLink("Torino", "Genova", 100);
-        assertTrue(150 == myHashmap.GetWeight("Torino", "Milano"));
-        assertTrue(100 == myHashmap.GetWeight("Torino", "Genova"));
-        assertFalse(100000 == myHashmap.GetWeight("Torino", "Milano"));
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
+        myGraph.AddLink("Torino", "Milano", 150);
+        myGraph.AddLink("Torino", "Genova", 100);
+        assertTrue(150 == myGraph.GetWeight("Torino", "Milano"));
+        assertTrue(100 == myGraph.GetWeight("Torino", "Genova"));
+        assertFalse(100000 == myGraph.GetWeight("Torino", "Milano"));
     }
 
     @Test
     public void CheckForNodes() throws Exception {
-        myHashmap.AddNode("Torino");
-        assertTrue(myHashmap.HasNode("Torino"));
-        assertFalse(myHashmap.HasNode("Genova"));
+        myGraph.AddNode("Torino");
+        assertTrue(myGraph.HasNode("Torino"));
+        assertFalse(myGraph.HasNode("Genova"));
     }
 
     @Test
     public void CheckDeleteNode() throws Exception {
-        myHashmap.AddNode("Torino");
-        myHashmap.AddNode("Milano");
-        myHashmap.AddNode("Genova");
-        myHashmap.AddLink("Milano", "Torino", 300);
-        myHashmap.DeleteNode("Milano");
-        assertFalse(myHashmap.HasNode("Milano"));
-        assertTrue(myHashmap.HasNode("Torino"));
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
+        myGraph.AddLink("Milano", "Torino", 300);
+        myGraph.DeleteNode("Milano");
+        assertFalse(myGraph.HasNode("Milano"));
+        assertTrue(myGraph.HasNode("Torino"));
         //TODO: Test if in the Milano node the node 'Torino' still exists or not. Shouldn't exists.
     }
 
@@ -77,32 +79,32 @@ public class UnitTest {
 
     @Test
     public void CheckNumberOfNodes() throws Exception {
-        myHashmap.AddNode("Torino");
-        myHashmap.AddNode("Milano");
-        myHashmap.AddNode("Genova");
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
 
-        assertTrue(3 == myHashmap.GetNumberOfNode());
-        assertFalse(4 == myHashmap.GetNumberOfNode());
+        assertTrue(3 == myGraph.GetNumberOfNode());
+        assertFalse(4 == myGraph.GetNumberOfNode());
     }
 
     @Test
     public void CheckNumberOfLinks() throws Exception {
-        myHashmap.AddNode("Torino");
-        myHashmap.AddNode("Milano");
-        myHashmap.AddNode("Genova");
-        myHashmap.AddLink("Torino", "Milano", 150);
-        myHashmap.AddLink("Torino", "Genova", 100);
-        assertTrue(2 == myHashmap.GetNumberOfLinks());
-        assertFalse(3 == myHashmap.GetNumberOfLinks());
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
+        myGraph.AddLink("Torino", "Milano", 150);
+        myGraph.AddLink("Torino", "Genova", 100);
+        assertTrue(2 == myGraph.GetNumberOfLinks());
+        assertFalse(3 == myGraph.GetNumberOfLinks());
     }
 
     @Test
     public void CheckGetNodes() throws Exception {
-        myHashmap.AddNode("Torino");
-        myHashmap.AddNode("Milano");
-        myHashmap.AddNode("Genova");
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
 
-        ArrayList<String> myNodes = myHashmap.GetNodes();
+        ArrayList<String> myNodes = myGraph.GetNodes();
 
         //done because the order on insertion is not allways the order of output to arrayList
         //to resolve this problem, the array list is first sorted and then checked by using a comparator
@@ -120,7 +122,23 @@ public class UnitTest {
 
     @Test
     public void CheckForDirect() throws Exception {
-        assertTrue(myHashmapDirect.IsDirect() == true);
-        assertTrue(myHashmapNotDirect.IsDirect() == false);
+        assertTrue(myGraphDirect.IsDirect() == true);
+        assertTrue(myGraphNotDirect.IsDirect() == false);
+    }
+
+    @Test
+    public void CheckKruskal() throws Exception {
+        myGraph.AddNode("Torino");
+        myGraph.AddNode("Milano");
+        myGraph.AddNode("Genova");
+        myGraph.AddLink("Milano", "Torino", 300);
+        Comparator<Integer> c = (w1,w2)->w1.intValue()-w2.intValue();
+        Kruskal<String, Integer> myKruskal = new Kruskal<String, Integer>(myGraph, c);
+        myKruskal.begin();
+        UnionFindSet<String> myNodes = myKruskal.getMstNodes();
+        ArrayList<Links<String, Integer>> myArrayList = myKruskal.getMstLinks();
+        Links<String, Integer> l = myArrayList.get(0);
+        assertTrue(l.getNode1()=="Milano");
+        assertTrue(l.getNode2()=="Torino");
     }
 }
